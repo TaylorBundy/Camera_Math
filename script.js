6fa29ef03948ae4c0ad38325cbc496b92314c3c0
TaylorBundy <martonbarbosa@hotmail.com>
2026-05-24 22:59:37 -0300

    terminado

--- script.js
+++ script.js
@@ -9,11 +9,39 @@ const listaNumeros = document.getElementById("listaNumeros");
 const resultadoElement = document.getElementById("resultado");
 
 let numerosDetectados = [];
+//const video = document.getElementById("video");
+
+const zoomInput = document.getElementById("zoom");
+
+let track;
+const zoomValor = document.getElementById("zoomValor");
+
+zoomInput.addEventListener("input", () => {
+  zoomValor.textContent = Number(zoomInput.value).toFixed(1) + "x";
+});
 
 // ==========================
 // INICIAR CAMARA
 // ==========================
 
+async function iniciarCamara2() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "environment",
+      },
+
+      audio: false,
+    });
+
+    video.srcObject = stream;
+    track = stream.getVideoTracks()[0];
+  } catch (error) {
+    alert("No se pudo abrir la cámara");
+
+    console.error(error);
+  }
+}
 async function iniciarCamara() {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -25,14 +53,86 @@ async function iniciarCamara() {
     });
 
     video.srcObject = stream;
-  } catch (error) {
-    alert("No se pudo abrir la cámara");
 
+    track = stream.getVideoTracks()[0];
+
+    configurarZoom();
+  } catch (error) {
     console.error(error);
+
+    alert("No se pudo abrir la cámara");
   }
 }
 
+// ==========================
+// CONFIGURAR ZOOM
+// ==========================
+
+function configurarZoom() {
+  const capabilities = track.getCapabilities();
+
+  console.log(capabilities);
+
+  // Verificar soporte
+  if (!capabilities.zoom) {
+    console.log("Zoom no soportado");
+
+    zoomInput.disabled = true;
+
+    return;
+  }
+
+  // Configurar slider
+  zoomInput.min = capabilities.zoom.min;
+
+  zoomInput.max = capabilities.zoom.max;
+
+  zoomInput.step = capabilities.zoom.step || 0.1;
+
+  zoomInput.value = capabilities.zoom.min;
+}
+// ==========================
+// EVENTO ZOOM
+// ==========================
+
+zoomInput.addEventListener("input", async () => {
+  if (!track) return;
+
+  try {
+    await track.applyConstraints({
+      advanced: [
+        {
+          zoom: Number(zoomInput.value),
+        },
+      ],
+    });
+  } catch (error) {
+    console.error("Error zoom:", error);
+  }
+});
+
+// ==========================
+// INICIAR
+// ==========================
+
 iniciarCamara();
+//const zoomInput = document.getElementById("zoom");
+
+// zoomInput.addEventListener("input", async () => {
+//   const capabilities = track.getCapabilities();
+
+//   if (!capabilities.zoom) return;
+
+//   await track.applyConstraints({
+//     advanced: [
+//       {
+//         zoom: Number(zoomInput.value),
+//       },
+//     ],
+//   });
+// });
+
+//iniciarCamara();
 
 // ==========================
 // ESCANEAR
@@ -46,7 +146,36 @@ btnCapturar.addEventListener("click", async () => {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    ctx.drawImage(video, 0, 0);
+    //ctx.drawImage(video, 0, 0);
+    const ancho = video.videoWidth;
+    const alto = video.videoHeight;
+
+    // RECORTE CENTRAL
+    const recorteAncho = ancho * 0.45;
+    const recorteAlto = alto * 0.9;
+
+    const x = (ancho - recorteAncho) / 2;
+
+    const y = (alto - recorteAlto) / 2;
+
+    canvas.width = recorteAncho;
+    canvas.height = recorteAlto;
+
+    ctx.drawImage(
+      video,
+
+      x,
+      y,
+
+      recorteAncho,
+      recorteAlto,
+
+      0,
+      0,
+
+      recorteAncho,
+      recorteAlto,
+    );
 
     // OCR
     const resultado = await Tesseract.recognize(canvas, "eng");
